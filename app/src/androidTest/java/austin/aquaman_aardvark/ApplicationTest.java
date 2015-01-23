5882367d41daa8e8f893950abc99a8f54e8def89
Austin Brown <atb.austin@gmail.com>
2015-01-22 21:02:54 -0600

    Austin
    Our project now has a running Android application!

---
+++ app/src/androidTest/java/austin/aquaman_aardvark/ApplicationTest.java
@@ -0,0 +1,13 @@
+package austin.aquaman_aardvark;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
