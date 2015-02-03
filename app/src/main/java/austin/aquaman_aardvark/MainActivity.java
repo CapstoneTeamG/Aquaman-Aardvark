47bd71e245b13c4685c1841153c6b341eaf486fc
Austin Brown <atb.austin@gmail.com>
2015-02-03 16:21:11 -0600

    Starting second part of tutorial.
    + DisplayMessageActivity

--- app/src/main/java/austin/aquaman_aardvark/MainActivity.java
+++ app/src/main/java/austin/aquaman_aardvark/MainActivity.java
@@ -4,9 +4,14 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.content.Intent;
+import android.view.View;
+import android.widget.EditText;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    public final static String EXTRA_MESSAGE = "austin.aquaman_aardvark.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,5 +40,19 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /** Used by sendButton  **/
+
+    public void sendMessage (View view) {
+        // TODO: Do something here
+
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+
+        startActivity(intent);
+
     }
 }
