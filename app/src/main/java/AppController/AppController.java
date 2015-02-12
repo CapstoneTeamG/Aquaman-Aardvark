e64fd680413a1dbfd0a68448ee130931542b139c
Austin Brown <atb.austin@gmail.com>
2015-02-11 22:09:44 -0600

    Sort Update!!
    http://www.androidhive.info/2014/07/android-custom-listview-with-image-and-text-using-volley/
    STEP 13
    
    + Working on implementing ListView

---
+++ app/src/main/java/AppController/AppController.java
@@ -0,0 +1,79 @@
+package AppController;
+
+/**
+ * Created by austin on 2/11/15.
+ */
+
+//TODO: WHAT?!?!?!
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+import android.text.TextUtils;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.Volley;
+
+public class AppController extends Application {
+
+
+    public static final String TAG = AppController.class.getSimpleName();
+
+    private RequestQueue mRequestQueue;
+//    private ImageLoader mImageLoader;
+
+    private static AppController mInstance;
+
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+        mInstance = this;
+    }
+
+    public static synchronized AppController getInstance()
+    {
+        return mInstance;
+    }
+
+    public RequestQueue getmRequestQueue()
+    {
+        if (mRequestQueue == null)
+            mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+
+        return mRequestQueue;
+    }
+
+//    public ImageLoader getImageLoader()
+//    {
+//        getmRequestQueue();
+//        if (mImageLoader == null)
+//        {
+           // For LruBitmapCache:
+           // http://www.androidhive.info/2014/07/android-custom-listview-with-image-and-text-using-volley/
+//            mImageLoader = new ImageLoader(this.mRequestQueue, new LruBitmapCache());
+//        }
+//
+//        return this.mImageLoader;
+//    }
+
+    public <T> void addToRequestQueue(Request<T> req, String tag)
+    {
+        // set the default tag if tag is empty
+        req.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
+        getmRequestQueue().add(req);
+    }
+
+    public <T> void addToRequestQueue (Request<T> req)
+    {
+        req.setTag(TAG);
+        getmRequestQueue().add(req);
+    }
+
+    public void cancelPendingRequests (Object tag)
+    {
+        if (mRequestQueue != null)
+            mRequestQueue.cancelAll(tag);
+    }
+}
